93c89e6a425cd8b65aee1164a11558b46d402781
Carsten Dannat <git@photoallee.de>
2022-11-05 15:21:05 +0100

    Sketch simplification and SDA / SCL definitions

--- Projects/LCD_Display/sketch_lcd_i2c_1602.ino
+++ Projects/LCD_Display/sketch_lcd_i2c_1602.ino
@@ -1,31 +1,26 @@
 #include <LiquidCrystal_I2C.h>
- 
+#include <Wire.h>
+
+#define SDA 13                    //Define SDA pins
+#define SCL 14                    //Define SCL pins
+
 // set the LCD number of columns and rows
 int lcdColumns = 16;
 int lcdRows = 2;
- 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
- 
-void setup(){
-  // initialize LCD
-  lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
+
+LiquidCrystal_I2C lcd(0x27,lcdColumns,lcdRows); 
+
+void setup() {
+  Wire.begin(SDA, SCL);           // attach the IIC pin
+  lcd.init();                     // LCD driver initialization
+  lcd.backlight();                // Open the backlight
+  lcd.setCursor(0,0);             // Move the cursor to row 0, column 0
+  lcd.print("hello, world!");     // The print content is displayed on the LCD
 }
- 
-void loop(){
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // print message
-  lcd.print("Hello, World!");
+
+void loop() {
+  lcd.setCursor(0,1);             // Move the cursor to row 1, column 0
+  lcd.print("Counter:");          // The count is displayed every second
+  lcd.print(millis() / 1000);
   delay(1000);
-  // clears the display to print new message
-  lcd.clear();
-  // set cursor to first column, second row
-  lcd.setCursor(0,1);
-  lcd.print("Hello, World!");
-  delay(1000);
-  lcd.clear(); 
 }
