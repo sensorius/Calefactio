280e76b18873ad2ff1be681214f8a7f7782d4ee7
Carsten Dannat <git@photoallee.de>
2022-11-07 20:10:59 +0100

    Additional debug output

--- Projects/LED_Builtin/sketch_led_builtin.ino
+++ Projects/LED_Builtin/sketch_led_builtin.ino
@@ -1,10 +1,13 @@
 #define LED_BUILTIN  2
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.print("Setup... ");
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   // Initialize serial interface
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("complete."); 
 }
 
 // the loop function runs over and over again forever
