6e69cd48a680a54ba8922602317df30940241bc8
Carsten Dannat <git@photoallee.de>
2022-11-07 20:12:19 +0100

    Fixed serial initialization

--- Projects/LED_Builtin/sketch_led_builtin.ino
+++ Projects/LED_Builtin/sketch_led_builtin.ino
@@ -1,11 +1,11 @@
 #define LED_BUILTIN  2
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // Initialize serial interface
+  Serial.begin(115200);
   Serial.print("Setup... ");
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  // Initialize serial interface
-  Serial.begin(115200);
   delay(1000);
   Serial.println("complete."); 
 }
