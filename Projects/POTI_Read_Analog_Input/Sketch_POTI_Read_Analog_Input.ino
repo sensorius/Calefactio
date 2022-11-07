937d3c6fc3a7987c4e962a9f1a0b942cd7b4c44f
Carsten Dannat <git@photoallee.de>
2022-11-07 20:54:19 +0100

    Calculation of analog value

--- Projects/POTI_Read_Analog_Input/Sketch_POTI_Read_Analog_Input.ino
+++ Projects/POTI_Read_Analog_Input/Sketch_POTI_Read_Analog_Input.ino
@@ -1,17 +1,30 @@
 // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
-const int potPin = 34;
+const int iPotPin = 34;
 
-// variable for storing the potentiometer value
-int potValue = 0;
+// variable to store the value coming from the sensor
+int iDigitalValue = 0;
+
+// variable to store the calculated analog value 
+float fAnalogVoltage = 0.0;
 
 void setup() {
+  
   Serial.begin(115200);
   delay(1000);
+  
 }
 
 void loop() {
-  // Reading potentiometer value
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
+  
+  // read the value from the analog channel
+  iDigitalValue = analogRead(iPotPin);
+  Serial.print("digital value = ");
+  Serial.print(iDigitalValue);      
+  
+  //convert digital value to analog voltage
+  fAnalogVoltage = (iDigitalValue * 3.3)/4095.0;
+  Serial.print("  analog voltage = ");
+  Serial.println(fAnalogVoltage);
   delay(1000);
+  
 }
