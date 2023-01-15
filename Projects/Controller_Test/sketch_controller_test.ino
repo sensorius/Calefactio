44e81fc2e1d07f891a74c215e33e08432f9186a0
Carsten Dannat <git@photoallee.de>
2023-01-15 13:48:18 +0100

    Data array implementation

--- Projects/Controller_Test/sketch_controller_test.ino
+++ Projects/Controller_Test/sketch_controller_test.ino
@@ -3,21 +3,37 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#include <WiFiUdp.h>
+
 // Webserver
 #include "WebServer.h"
 
 
 
 #define LED_BUILTIN  2  // Blue LED on ESP32 board
-
+#define MAX_FORECAST_COUNT 10
 
 // WLAN credentials
 const char* pSSID = "YOUR_SSID";
 const char* pPassword = "YOUR_PASSWORD";
 
-String strSpotPrice = "1.09";              
-String strWindSpeed = "4.2";
-String strOutdoorTemp = "-5.3";
+
+#define MAX_FORECAST_COUNT 10
+
+typedef struct {
+  String strSpotPrice = "0";
+  String strWindSpeed = "0";
+  String strOutdoorTemp = "0";
+} forecastDataType;
+
+forecastDataType forecastData[MAX_FORECAST_COUNT];
+
+String strSpotPriceInit[MAX_FORECAST_COUNT] = { "0.74", "0.86", "1.07", "1.44", "1.68", "1.73", "1.42", "1.21", "1.12", "1.04"};
+String strWindSpeedInit[MAX_FORECAST_COUNT] = { "6", "6", "5", "5", "4", "4", "5", "5", "6", "6"};
+String strOutdoorTempInit[MAX_FORECAST_COUNT] = { "9", "8", "7", "6", "6", "5", "5", "4", "4", "4"};
+
+
+
 String strHeatingBufferTemp = "63.1";
 String strHeatingBufferFakeTemp = "0.0";
 
@@ -50,8 +66,6 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi, my IP Address is: ");
   Serial.println(WiFi.localIP());
-  
-  
   Serial.print("Wifi RSSI=");
   Serial.println(WiFi.RSSI());
 
@@ -66,6 +80,13 @@ void setup() {
   
   delay(1000);
 
+  for( int i=0; i<MAX_FORECAST_COUNT; i++ ) {
+    forecastData[i].strSpotPrice = strSpotPriceInit[i];
+    forecastData[i].strWindSpeed = strWindSpeedInit[i];
+    forecastData[i].strOutdoorTemp = strOutdoorTempInit[i];
+    Serial.println("i="+String(i)+":"+forecastData[i].strSpotPrice);
+    }
+
 }
 
 
@@ -77,32 +98,73 @@ void wshandle_OnConnect() {
   String strHTML = "<!DOCTYPE html> <html>"\
   "<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>"\
   "<title>Calefactio</title>"\
-  "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}"\
+  "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center; }"\
   "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}"\
   "p {font-size: 14px;color: #888;margin-bottom: 10px;}</style>"\
   "\n</head><body>"\
   "<h1>Controller Test</h1>";
 
-
+  strHTML += "<center>";
   strHTML +="<h4><form action=\"/calc\">";
-  strHTML +="Heating buffer size [l]: <input type=number name=\"heatingBufferSize\" value="+strHeatingBufferSize+" min=0 max=10000 step=1><br>";
-  strHTML +="Electricity import charge [kr&#47;kWh]: <input type=number name=\"electricityImportCharge\" value="+strElectricityImportCharge+" min=0 max=100 step=0.01><br>";
-  strHTML +="<br>Spot price [kr]: <input type=number name=\"spotPrice\" value="+strSpotPrice+" min=0 max=100 step=0.01><br>";
-  strHTML +="Wind speed [m&#47;s]: <input type=number name=\"windSpeed\"  value="+strWindSpeed+" min=0 max=130 step=0.1><br>";
-  strHTML +="Outdoor temperature [&#176;C]: <input type=number name=\"outdoorTemp\" value="+strOutdoorTemp+" min=-50 max=50 step=0.1><br>";
-  strHTML +="<br>Heating buffer temperature [&#176;C]: <input type=number name=\"heatingBufferTemp\" value="+strHeatingBufferTemp+" min=0 max=100 step=0.1><br>";
-  strHTML +="<br><input type=\"submit\" value=\"Calculate\">";
+
+
+  strHTML +="Heating buffer size [l]: <input type=number name=\"heatingBufferSize\" value="+strHeatingBufferSize+" min=0 max=10000 step=1>";
+  strHTML +="<br>Electricity import charge [kr&#47;kWh]: <input type=number name=\"electricityImportCharge\" value="+strElectricityImportCharge+" min=0 max=100 step=0.01>";
+  strHTML +="<br>Heating buffer temp. [&#176;C]: <input type=number name=\"heatingBufferTemp\" value="+strHeatingBufferTemp+" min=0 max=100 step=0.1><br>";
+  
+  strHTML += "<br><table>";
+  strHTML += "<tr><th>Data set</th>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    strHTML += "<th>"+String(i)+"</th>";
+  } strHTML += "</tr>";
+
+
+  strHTML += "<tr><td>Wind speed [m&#47;s]:</td>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    String strName = "windSpeed_"+String(i);
+    strHTML += "<td><input type=number name=\""+strName+"\" value="+forecastData[i].strWindSpeed+" min=0 max=130 step=0.1></td>";
+  } strHTML += "</tr>";
+
+
+  strHTML += "<tr><td>Outdoor temp. [&#176;C]:</td>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    String strName = "outdoorTemp_"+String(i);
+    strHTML += "<td><input type=number name=\""+strName+"\" value="+forecastData[i].strOutdoorTemp+" min=-50 max=50 step=0.1></td>";
+  } strHTML += "</tr>";
+
+
+  strHTML += "<tr><td>Spot price [kr]:</td>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    String strName = "spotPrice_"+String(i);
+    strHTML += "<td><input type=number name=\""+strName+"\" value="+forecastData[i].strSpotPrice+" min=0 max=100 step=0.01></td>";
+  } strHTML += "</tr>";
+
+  strHTML += "</table>";
+
+
+
+  strHTML +="<br><input type=\"submit\" value=\"Calculate...\">";
   strHTML +="</form></h4><br>";
 
-  if( strHeatingBufferFakeTemp == "0.0" ) {
-    strHTML += "";
-    }
-  else {
-    strHTML += "Calculated data:<br>";
-    strHTML += "<br>Probability of wind electricity: "+ String(fProbabilityWindElectricity,1);
-    strHTML += "<br>Temperature of heating buffer: "+ strHeatingBufferFakeTemp + "&#176;C";
-    }
+  
 
+  strHTML += "<br><table>";
+  strHTML += "<tr><th>Forecast set</th>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    strHTML += "<th>"+String(i)+"</th>";
+  } strHTML += "</tr>";
+
+
+  strHTML += "<tr><td>Probability wind electricity:</td>";
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    String strValue = String(calcProbabilityWindElectricity(forecastData[i].strWindSpeed),1);
+    strHTML += "<td>"+strValue+"</td>";
+  } strHTML += "</tr>";
+
+  strHTML += "</table>";
+
+
+  strHTML += "</center>";
 
   strHTML += "</body></html>"; 
   
@@ -113,29 +175,25 @@ void wshandle_OnCalc() {
 
   Serial.print("ARGS: ");
   Serial.println(server.args());
-  Serial.println("Data: " + server.arg("spotPrice"));
-  Serial.println("Data: " + server.arg("windSpeed"));
-  Serial.println("Data: " + server.arg("outdoorTemp"));
-  Serial.println("Data: " + server.arg("heatingBufferTemp"));
 
-  strWindSpeed = server.arg("windSpeed");
-  strSpotPrice = server.arg("spotPrice");
-  strOutdoorTemp = server.arg("outdoorTemp");
   strHeatingBufferTemp = server.arg("heatingBufferTemp");
-
   strHeatingBufferSize = server.arg("heatingBufferSize");
   strElectricityImportCharge = server.arg("electricityImportCharge");
+
+  for(int i=0; i<MAX_FORECAST_COUNT; i++) {
+    String strName = "outdoorTemp_"+String(i); forecastData[i].strOutdoorTemp = server.arg(strName);
+    strName = "windSpeed_"+String(i); forecastData[i].strWindSpeed = server.arg(strName);
+    strName = "spotPrice_"+String(i); forecastData[i].strSpotPrice = server.arg(strName);
+  }
  
   float fHeatingBufferTemp = 76.4;
-
   strHeatingBufferFakeTemp =  String(fHeatingBufferTemp,1);
-
-  fProbabilityWindElectricity = calcProbabilityWindElectricity(strWindSpeed);
 
   server.sendHeader("Location", "/",true);  
   server.send(302, "text/plain", "");
   
 }
+
 
 
 void wshandle_NotFound(){
